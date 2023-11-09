976a630db7b3c1f35a59c86526fadcece09aa6cb
YoonMin <dbsals999@naver.com>
2023-11-09 21:04:13 +0900

    서블릿으로 구구단 만들기

--- src/main/java/ym/jsp/board/servlet/GugudanServlet.java
+++ src/main/java/ym/jsp/board/servlet/GugudanServlet.java
@@ -8,14 +8,21 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/hello")
-public class HelloServlet extends HttpServlet {
+@WebServlet("/gugudan")
+public class GugudanServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     req.setCharacterEncoding("UTF-8");
     resp.setCharacterEncoding("UTF-8");
     resp.setContentType("text/html; charset-ut-8");
 
-    resp.getWriter().append("안녕22222");
+    int dan =  Integer.parseInt(req.getParameter("dan"));
+    int limit =  Integer.parseInt(req.getParameter("limit"));
+
+    resp.getWriter().append("<h1>%d단</h1>\n".formatted(dan));
+
+    for (int i = 1; i <= limit; i++) {
+      resp.getWriter().append("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
+    }
   }
 }
